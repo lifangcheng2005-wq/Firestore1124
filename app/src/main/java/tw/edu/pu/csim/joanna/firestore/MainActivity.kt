f5682ba0711915de3b5d45627e878e2da6499893
joanna <lifangcheng2005@gmail.com>
2025-11-24 16:23:39 +0800

    v.2新增資料

--- app/src/main/java/tw/edu/pu/csim/joanna/firestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/joanna/firestore/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import tw.edu.pu.csim.joanna.firestore.ui.theme.FirestoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirestoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UserScoreScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirestoreTheme {
-        Greeting("Android")
+fun UserScoreScreen( userScoreViewModel: UserScoreViewModel = viewModel()
+) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            // 在按鈕點擊時，直接呼叫 ViewModel 的函式
+            var userScore = UserScoreModel("子青", 39)
+            userScoreViewModel.addUser(userScore)
+        }) {
+            Text("新增資料")
+        }
+        Text(userScoreViewModel.message)
     }
 }
